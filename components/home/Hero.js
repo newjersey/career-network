0023ceacf14a9af30dccb04b217a752f50e0f490
Ross Dakin <rossdakin@gmail.com>
2019-04-30 13:27:24 -0400

    Add hero image
    May want to use an object vs. background-image later if we want to animate it

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -5,11 +5,25 @@ import Typography from '@material-ui/core/Typography';
 import CircumstancePicker from './CircumstancePicker'
 
 const styles = theme => ({
+  root: {
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'right',
+    [theme.breakpoints.up('sm')]: {
+      backgroundImage: 'url(/static/img/index/hero.svg)',
+    },
+    [theme.breakpoints.up('md')]: {
+      minHeight: '30vw',
+    },
+  },
   title: {
     marginTop: theme.spacing.unit * 1,
     marginBottom: theme.spacing.unit * 3,
     [theme.breakpoints.down(350)]: {
       fontSize: '1.8rem',
+    },
+    [theme.breakpoints.only('sm')]: {
+      maxWidth: '56%',
     },
   },
   highlight: {
@@ -17,14 +31,14 @@ const styles = theme => ({
   },
   picker: {
     marginBottom: theme.spacing.unit * 10,
-  }
+  },
 });
 
 function Hero(props) {
   const { classes } = props;
 
   return (
-    <React.Fragment>
+    <div className={classes.root}>
       <Typography variant="h6" component="h3">
         You're not alone
         </Typography>
@@ -32,7 +46,7 @@ function Hero(props) {
         It takes a <span className={classes.highlight}>network</span> to find the right <span className={classes.highlight}>career</span>
       </Typography>
       <CircumstancePicker className={classes.picker} />
-    </React.Fragment>
+    </div>
   );
 }
 
