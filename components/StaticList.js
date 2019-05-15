d9d69fb7e1dd46192343d3ccf3b8a8596ba28f6b
Ross Dakin <rossdakin@gmail.com>
2019-05-15 13:05:15 -0400

    Create structure for static pages

---
+++ components/StaticList.js
@@ -0,0 +1,50 @@
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+});
+
+class StaticList extends React.Component {
+  render() {
+    const { classes, children, title, nesting } = this.props;
+    const titleProps = {
+      variant: `h${nesting + 4}`,
+      component: `h${nesting + 2}`,
+    };
+
+    return (
+      <div className={classes.root}>
+        <Typography {...titleProps}>{title}</Typography>
+        {children &&
+          <ul>
+            {children.map(node =>
+              <li>
+                <Typography>
+                  {node}
+                </Typography>
+              </li>
+            )}
+          </ul>
+        }
+      </div>
+    );
+  }
+}
+
+StaticList.propTypes = {
+  classes: PropTypes.object.isRequired,
+  chldren: PropTypes.arrayOf(PropTypes.node),
+  title: PropTypes.string.isRequired,
+  nesting: PropTypes.number,
+};
+
+StaticList.defaultProps = {
+  nesting: 0,
+}
+
+export default withStyles(styles)(StaticList);
--- pages/networking.js
+++ pages/networking.js
@@ -4,11 +4,12 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 import ScaffoldContainer from '../components/ScaffoldContainer';
+import StaticList from '../components/StaticList';
+import { Link } from '@material-ui/core';
 
 const styles = theme => ({
   root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 10,
+    paddingTop: theme.spacing.unit * 5,
   },
 });
 
@@ -19,7 +20,43 @@ class Networking extends React.Component {
     return (
       <div className={classes.root}>
         <ScaffoldContainer>
-          <Typography variant="h1">Networking.</Typography>
+          <Typography variant="h3" component="h1">Relationship-Building Resources</Typography>
+
+          <Typography variant="body1">
+            Developing new connections and deepening your existing relationships are KEY to your job search.
+          </Typography>
+          <Typography variant="body1">
+            Not only are you more likely to find out about new opportunities, people, and companies when you broaden your network, many organizations are explicitly looking for you to bring new connections to their business needs.
+          </Typography>
+          <Typography variant="body1">
+            Although it’s something many job seekers hate, relationship building and meeting new people are the two things you need to be spending the most time on in your job search.
+          </Typography>
+
+          <StaticList title="Relationship-Building Goals in Job Search">
+            <React.Fragment>
+              <strong>Relationship-Building Goals in Job Search</strong> –
+              People you don’t know well. Research shows that new connections are the ones most likely to bring you new opportunities.
+            </React.Fragment>
+            <React.Fragment>
+              <strong>Deepen your existing connections</strong> –
+              Make efforts to do things for people in your existing network that bring value to them. Sharing resources, offering to connect them to someone in your network, and helping them accomplish a goal can help you build positive relationships with the people you know.
+            </React.Fragment>
+          </StaticList>
+
+          <StaticList title="Resources to Help" />
+
+          <StaticList title="Webinars (from NSCN)" nesting={1}>
+            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+          </StaticList>
+
+          <StaticList title="Webinars (from NSCN)" nesting={1}>
+            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+          </StaticList>
+
         </ScaffoldContainer>
       </div>
     );
