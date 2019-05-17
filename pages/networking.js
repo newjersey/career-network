e9567201d6d8a47a12f743ece59644ff80e43c8a
T-1000 <t-1000@10-18-194-254.dynapool.wireless.nyu.edu>
2019-05-17 15:51:51 -0400

    New static pages

--- pages/networking.js
+++ pages/networking.js
@@ -41,21 +41,135 @@ class Networking extends React.Component {
               <strong>Deepen your existing connections</strong> –
               Make efforts to do things for people in your existing network that bring value to them. Sharing resources, offering to connect them to someone in your network, and helping them accomplish a goal can help you build positive relationships with the people you know.
             </React.Fragment>
+            <React.Fragment>
+              <strong>Work on interesting projects so you have something to talk about besides looking for work! </strong> –
+              Research trends in your industry or occupation, write white papers or mini reports, take on a leadership role in a volunteer organization, or start a mastermind group. Set aside time to work on things that you find interesting and that will give you something to talk about when you engage with both new acquaintances as well as people in your current network.            </React.Fragment>
+          </StaticList>
+
+          <StaticList title="Relationship Basics">
+            <React.Fragment>
+              <strong>Don’t move too fast! </strong> –
+              One of the biggest mistakes job seekers make is trying to move too quickly in the relationship. People have to know, like and trust you before they are going to be willing to give you referrals or leads, so pay attention to growing a genuine relationship before you start asking them for leads.            </React.Fragment>
+            <React.Fragment>
+              <strong>Focus on what you can GIVE, not on what you can GET.</strong> –
+              Look for ways to help other people, including sharing resources, information, and connections. When you meet new people, think about contributions you can make to their lives or well being. Do the same with people you already know. Not only does this improve your relationships, it also makes you feel better.
+            </React.Fragment>
+            <React.Fragment>
+              <strong>Ask questions that convey interest in the other person.</strong> –
+              When you’re job searching, you can become so focused on your job search that you are either selling yourself or only asking questions that are related to your needs. But relationships are two-way. You need to remind yourself to ask questions that convey your interest in the person you are talking to, learning about what they are doing or what’s important to them, etc. This helps you identify ways that you can help them, which creates a more positive relationship.
+            </React.Fragment>
+            <React.Fragment>
+              <strong>Try to meet people through avenues you care about. </strong> –
+              participate in meetings or groups where you can meet other people who share those interests. <a href="meetup.com">Meetup.com</a> is a great way to find other people who share your interests. Join professional groups on LinkedIn, contribute to conversations, and reach out to members in the group who seem interesting or with whom you could share a resource or idea that could help them.
+            </React.Fragment>
+            <React.Fragment>
+              <strong>Set goals to meet new people each week. </strong> –
+              Challenge yourself to meet 5 new people a week, or even 10! That’s only one or two new people a day, five days a week. Having a specific numeric goal to accomplish will make it more likely that you do it.            </React.Fragment>
+            <React.Fragment>
+              <strong>Set weekly goals to reach out to people who are already in your network.  </strong> –
+              Try starting your day by sending an email to someone you know, thanking them for some way they have contributed to your life. Or spend an hour every Friday afternoon, sending check-in emails or making catch-up calls with people you haven’t talked to for awhile. Or spend some time writing unsolicited recommendations for people you are connected to on LinkedIn.            </React.Fragment>
+            <React.Fragment>
+              <strong>When you meet new people, connect with them on LinkedIn by sending a personalized invitation.<a href="https://www.linkedin.com/help/linkedin/answer/46662/personalizing-invitations-to-connect?lang=en">(here's how)</a> </strong> –
+              Be sure to include something you appreciate about your interaction with them or something you have in common that you noticed on their profile.
+                          </React.Fragment>
+            <React.Fragment>
+              <strong>Don’t just network with other unemployed people.</strong> –
+              Many unemployed job seekers have a tendency to connect with other people who are also unemployed. While this can be great for support, it isn’t always good for your search. Find ways to meet and connect with people who are currently working, even if it feels uncomfortable.
+             </React.Fragment>
+            <React.Fragment>
+              <strong>Age-diversify your network.</strong> –
+              Older job seekers in particular tend to have networks that skew older. But it’s people in their 30’s and 40’s who may be in the hiring manager position. Make a specific effort to reach out to people who are younger than you as they may be in the best position to help.
+             </React.Fragment>
           </StaticList>
 
           <StaticList title="Resources to Help" />
 
           <StaticList title="Webinars (from NSCN)" nesting={1}>
             <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
-            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
-            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+            <Link href="https://vimeo.com/335218945">Why Your Networking Isn’t Working and What to Do About It</Link>
+            <Link href="https://vimeo.com/257006264">Connecting Without Networking to Thrive in the Gig Economy</Link>
           </StaticList>
 
-          <StaticList title="Webinars (from NSCN)" nesting={1}>
-            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
-            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
-            <Link href="https://vimeo.com/182588364">Networking for People Who Hate Networking</Link>
+          <StaticList title="General Relationship/Building Resources" nesting={1}>
+            <Link href="https://www.lifeoptimizer.org/2007/06/06/106-tips-to-become-a-master-connector/">106 Tips To Become a Master Connector</Link>
+            <Link href="http://www.thelaunchcoach.com/libraryfiles/7_Steps_To_Networking_Your_Way_To_A-Listers_Fast.pdf">66 New Ideas for Upping Your Networking Game</Link>
+            <Link href="https://www.themuse.com/advice/66-new-ideas-for-upping-your-networking-game">Networking for People Who Hate Networking</Link>
+            <Link href="https://drive.google.com/file/d/0B9jWVyCVFZu6bnhrYVpra1UyazQ/view?usp=sharing">The Art of Asking Workbook</Link>
+            <Link href="https://idealistcareers.org/20-ways-to-network-that-dont-feel-like-networking/">20 Ways to Network that Don’t Feel Like Networking</Link>
           </StaticList>
+
+          <StaticList title="Documents to Help With Your Networking" nesting={1}>
+            <Link href="https://www.resumeworded.com/networking-email-templates/">Sample Email Networking Templates, including follow-ups and thank yous</Link>
+            <Link href="https://www.careercontessa.com/advice/back-to-basics-networking/">How to Write Every Type of Professional Networking Email</Link>
+            <Link href="https://vimeo.com/286052527">Beyond the Resume: Networking Documents that Can Help You Get Your Foot in the Door (Webinar)</Link>
+            <Link href="https://www.careercontessa.com/advice/personal-business-card/">Personal Business Cards and Templates</Link>
+          </StaticList>
+
+          <StaticList title="Your Networking “Pitch”" nesting={1}>
+            <Link href="http://timsstrategy.com/6-easy-steps-to-a-great-elevator-pitch/">
+              6 Easy Steps to a Great Elevator Pitch (with examples)
+            </Link>
+            <Link href="https://www.thebalancecareers.com/elevator-speech-examples-and-writing-tips-2061976">
+              How to Create an Elevator Pitch with Examples
+            </Link>
+            <Link href="https://www.businessinsider.com/how-to-create-elevator-pitch-2017-5">
+              A Simple Formula for Crafting a Networking Pitch
+            </Link>
+            <Link href="https://www.roberthalf.com/blog/job-market/3-elevator-speech-examples-for-the-job-hunt">
+              3 Elevator Speech Examples for the Job Search
+          </Link>
+          </StaticList>
+
+          <StaticList title="Connecting on LinkedIn" nesting={1}>
+          <Link href="https://www.weidert.com/whole_brain_marketing_blog/best-ways-to-gain-more-connections-on-linkedin">
+            9 Best Ways to Grow Your Network on LinkedIn
+          </Link>
+          <Link href="https://www.themuse.com/advice/want-people-to-accept-your-linkedin-requests-use-these-10-templates">
+            10 Templates to Get People to Accept Your LinkedIn Connection Request
+          </Link>
+          <Link href="https://www.themuse.com/advice/how-to-take-your-linkedin-relationship-to-the-next-levela-coffee-date">
+            How to Take Your LinkedIn Relationship to the Next Level
+          </Link>
+          <Link href="https://www.recruiter.com/i/the-ultimate-linkedin-guide-part-3-engaging-with-your-network/">
+            How to Engage with Your LinkedIn Network
+          </Link>
+          </StaticList>
+
+          <StaticList title="Connecting at Networking Events" nesting={1}>
+  
+          <Link href="https://www.thebalancecareers.com/how-to-find-networking-events-worth-going-to-4178849">How to Find Networking Events Worth Going To</Link>
+          <Link href="https://www.inc.com/melanie-deziel/6-surefire-tips-for-making-the-most-of-your-next-networking-event.html">6 Ways to Prepare for a Networking Event</Link>
+          <Link href="https://www.thebalancecareers.com/perfect-career-networking-conversation-starters-4584702">Perfect Networking Conversation Starters</Link>
+          <Link href="https://www.themuse.com/advice/6-ways-to-successfully-make-connections-even-if-you-hate-networking-events">6 Ways to Successfully Make Connections Even if You Hate Networking Events</Link>
+          <Link href="https://www.thebalancecareers.com/follow-up-letter-to-a-contact-met-at-a-networking-event-2063486">How to Follow Up After Networking Events with Examples</Link>
+
+          </StaticList>
+
+          <StaticList title="Informational Interviews" nesting={1}>
+
+          <Link href="https://www.thebalancecareers.com/how-an-informational-interview-can-help-your-career-2058564">How an Informational Interview Can Boost Your Career</Link> 
+          <Link href="https://www.themuse.com/advice/how-to-ask-for-an-informational-interview-and-get-a-yes">How to Ask for an Informational Interview and Get a Yes</Link>
+          <Link href="https://www.careercontessa.com/advice/perfect-informational-interview/">The Perfect Informational Interview Template: A 5-Step Guide</Link>
+          <Link href="https://biginterview.com/blog/2011/08/informational-interview-tips.html">Informational Interview Tips for Success</Link>
+
+          </StaticList>
+
+          <StaticList title="Start Your Own Networking Event" nesting={1}>
+
+          <Link href="https://biginterview.com/blog/2011/08/informational-interview-tips.html">Start a Working Out Loud Circle</Link>
+          <Link href="https://www.lifehack.org/articles/featured/how-to-start-and-run-a-mastermind-group.html">Start a Mastermind Group</Link>
+          <Link href="https://linkednlocal.com/become-a-host/">Start a LinkedIn Local Group</Link>
+          <Link href="http://www.thegenerositynetwork.com/resources/jeffersonian-dinners/">Have a Jeffersonian Dinner (Dinner with shared purpose & conversation)</Link>
+          <Link href="https://www.forbes.com/sites/alisoncoleman/2018/09/24/when-you-cant-find-the-right-networking-group-start-one-of-your-own/#7dba0377468b">When You Can’t Find the Right Networking Group, Start One Of Your Own</Link>
+
+          </StaticList>
+
+          <StaticList title="Getting Referrals" nesting={1}>
+          <Link href="https://www.glassdoor.com/blog/how-to-ask-for-a-referral/">How to Ask for a Referral</Link>
+          <Link href="https://www.thebalancecareers.com/how-to-ask-for-a-job-referral-2062989">How to Ask for Job Referral</Link>
+
+          </StaticList>
+
 
         </ScaffoldContainer>
       </div>
---
+++ pages/target-company.js
@@ -0,0 +1,112 @@
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+import ScaffoldContainer from '../components/ScaffoldContainer';
+import StaticList from '../components/StaticList';
+import { Link } from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 5,
+  },
+});
+
+class Networking extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <ScaffoldContainer>
+          <Typography variant="h3" component="h1">Identifying/Researching Target Companies Resources:
+</Typography>
+
+          <Typography variant="body1">
+            A major portion of your job search time should be spent on identifying and researching target companies and making connections to individuals within those companies. In this section, you will learn what to look for and how to find information so you can have a more targeted and effective job search plan.
+          </Typography>
+          <Typography variant="body1">
+            Identifying  target company will help guide your job search with direction and focus.
+          </Typography>
+          <Typography variant="body1">
+            Researching target company will effectively narrow your job search.
+          </Typography>
+          <Typography variant="body1">
+            Reviewing the “Careers” section of target companies will help you effectively search for positions you are qualified for within the companies you are interested in
+          </Typography>
+          <Typography variant="body1">
+            Although it’s something many job seekers hate, identifying and researching target companies are two high-priority tasks that will make your job search more productive, more effective and yield better results.
+          </Typography>
+
+
+          <StaticList title="The Basic Steps to Identifying/Researching Target Companies: ">
+            <React.Fragment>
+              <strong>Make a list of target companies</strong> –
+              be strategic about the companies and the job opportunities you want to pursue. There are several ways you can begin to develop a list of companies you want to target in your job search, including using job postings, looking up companies on LinkedIn, and through conversations with people in your network. Every time you hear of a new company that might hire people with your background and experience, you should add them to your list so that you can do some further research.            </React.Fragment>
+            <React.Fragment>
+              <strong>Find  information on those target companies</strong> –
+              do your research. Start with the company website. You can also look up the company on LinkedIn and do a Google search to discover more information. As you gather information about your target companies, keep notes on what you discover, especially as the information may apply to how you can bring value to the company. Also pay attention to whether the company is aligned with your personal values, passions, etc.             </React.Fragment>
+              <React.Fragment>
+              <strong>Find job listings at those target companies that are suitable for you </strong> –
+              look for opportunities at your target companies that would fit your background and skills. </React.Fragment>
+              <React.Fragment>
+              <strong>Identify contacts at those target companies </strong> –
+              targeted job search and targeted networking is critical to job search. As you are researching companies that interest you, you also want to begin developing a list of contacts within those companies. Ideally, these are people you already know. But you will also want to identify hiring managers, other people in departments you might work with, etc.  LinkedIn is a great resource to find connections.  For each target company you’ve identified, continue to compile a list of contacts within that company.              </React.Fragment>
+              <React.Fragment>
+              <strong>Reach out to those contacts and start to build relationships or work on existing relationships </strong> –
+              expand your connections or deepen existing ones at the target companies you want to pursue. Keep track of the connections, the contact you have made with these connections, and be sure to always follow up. </React.Fragment>
+              <React.Fragment>
+              <strong>Create a targeted job search plan  </strong> –
+              your plan should consist of specific job opportunities at specific companies and specific connections you can turn to to help get your foot in the door. </React.Fragment>
+          </StaticList>
+
+          <StaticList title="Resources to Help" />
+
+          <StaticList title="Webinars (from NSCN)" nesting={1}>
+            <Link href="https://vimeo.com/241749119">Identifying and Researching Target Companies</Link>
+            <Link href="https://vimeo.com/325491307">Creating Targeted Job Search Plans</Link>
+          </StaticList>
+
+          <StaticList title="Identifying Company Targets" nesting={1}>
+            <Link href="https://www.thebalancecareers.com/creating-a-target-list-of-companies-2060032">How to Create a Target List of Companies</Link>
+            <Link href="https://www.careerattraction.com/job-searching-20-creative-ways-to-build-your-target-company-list/">Job Searching? 20 Creative Ways to Build Your Target Company List</Link>
+            <Link href="https://executivecareerbrand.com/7-tips-to-build-your-executive-job-search-target-companies-list/">7 Tips to Build Your Executive Job Search Target Companies List</Link>
+            <Link href="https://www.topresume.com/career-advice/create-target-company-list-for-job-search">How to Build a Company List of Top Employers for Your Job Search</Link>
+            <Link href="https://premium.linkedin.com/content/premium/global/en_us/index/jobsearch/resources/get-connected/how-to-build-a-list-of-target-companies">How to Build a List of Target Companies</Link>
+            <Link href="https://www.linkedin.com/directory/companies/">LinkedIn Companies Directory</Link>
+          </StaticList>
+
+          <StaticList title="Research Company Targets" nesting={1}>
+            <Link href="https://www.thebalancecareers.com/how-to-research-a-company-2058508">How and Why to Research a Company</Link>
+            <Link href="https://www.aol.com/2009/04/01/why-is-company-research-so-important/">Why is Company Research So Important?</Link>
+            <Link href="https://www.youtube.com/watch?v=1A9bLHyBw94">How to Conduct Company Research for your Job Search</Link>
+            <Link href="https://biginterview.com/blog/2014/10/company-research-job-interview.html">The Job-Seeker's Guide to Company Research</Link>
+            <Link href="https://executivecareerbrand.com/best-ways-and-places-to-research-your-target-employers/">Best Ways and Places to Research Your Target Employers</Link>
+            <Link href="https://www.flexjobs.com/blog/post/job-search-tactic-start-with-companies-not-job-listings/">Job Search Tactic: Start with Companies, Not Job Listings</Link>
+            <Link href="https://www.indeed.com/career-advice/finding-a-job/the-complete-guide-to-researching-a-company">The Complete Guide to Researching a Company</Link>
+            <Link href="https://www.glassdoor.com/Salaries/index.htm">Glassdoor</Link>
+          </StaticList>
+
+          <StaticList title="Identifying and Researching Company Contacts" nesting={1}>
+            <Link href="https://www.forbes.com/sites/carolinecenizalevine/2016/03/28/how-to-find-a-contact-at-your-dream-company-six-ways-to-get-insider-information/#7d10cd897698">How To Find A Contact At Your Dream Company: Six Ways To Get Insider Information</Link>
+            <Link href="https://www.thebalancecareers.com/how-to-find-contacts-at-a-company-2062589">How to Find Contacts at a Company</Link>
+            <Link href="https://www.themuse.com/advice/how-to-find-an-in-at-your-dream-companyfast">How to Find an “In” at Your Dream Company—Fast</Link>
+            <Link href="https://www.theladders.com/career-advice/how-to-make-inside-connections">How to Create Inside Connections with Companies You Want to Work For</Link>
+            <Link href="https://www.socialmediaexaminer.com/how-to-find-and-connect-with-target-prospects-on-linkedin/">How to Find and Connect With Target Prospects on LinkedIn</Link>
+            <Link href="https://blog.linkedin.com/2018/february/21/tips-for-searching-like-a-pro-on-linkedin">Tips for Searching Like a Pro on LinkedIn</Link>
+            <Link href="https://www.linkedin.com/directory/companies/">LinkedIn Companies Directory</Link>
+            <Link href="https://www.linkedin.com/help/linkedin/answer/4326/searching-your-connection-s-connections?lang=en">Searching Your Connection's Connections</Link>
+          </StaticList>
+
+        </ScaffoldContainer>
+      </div>
+    );
+  }
+}
+
+Networking.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Networking);
