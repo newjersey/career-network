newjersey/career-network

commit 29efcf994257208ae805b1050fe8262b5c3cd18b

Cleanup to pass linting

components/Firebase/firebase.js
@@ -12,10 +12,12 @@ class Firebase {
     this.uiConfig = uiConfig;
   }
 
-  signOut = () => this.auth.signOut();
+  signOut() {
+    this.auth.signOut();
+  }
 
-  onAuthStateChanged = (callback) =>
-    this.auth.onAuthStateChanged(authUser => {
+  onAuthStateChanged(callback) {
+    this.auth.onAuthStateChanged((authUser) => {
       // if (authUser) {
       //   this.user(authUser.uid)
       //     .once('value')
@@ -42,6 +44,7 @@ class Firebase {
 
       callback(authUser);
     });
+  }
 }
 
 export default Firebase;

components/AnimatedSVG.js
@@ -10,32 +10,34 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function AnimatedSVG(props) {
-  const { path } = props;
+  const { path, transform } = props;
   const classes = useStyles();
   const domNode = useRef();
 
-  const handleScroll = () => {
-    const scrollPercentage = (
-      (document.documentElement.scrollTop + document.body.scrollTop) /
-      (document.documentElement.scrollHeight - document.documentElement.clientHeight));
-
-    // Page scroll percentage is a hacky metric to use here.
-    // TODO: calculate and expose some smarter calculations.
-    const transform = props.transform(scrollPercentage);
-    const svgElements = domNode.current.contentDocument.querySelectorAll('.animate');
-
-    svgElements.forEach((el) => {
-      el.setAttributeNS(null, 'transform', transform);
-    });
-  };
-
   useEffect(() => {
+    const handleScroll = () => {
+      const scrollPercentage = (
+        // eslint-disable-next-line operator-linebreak
+        (document.documentElement.scrollTop + document.body.scrollTop) /
+        (document.documentElement.scrollHeight - document.documentElement.clientHeight));
+
+      // Page scroll percentage is a hacky metric to use here.
+      // TODO: calculate and expose some smarter calculations.
+      const transformValue = transform(scrollPercentage);
+      const svgElements = domNode.current.contentDocument.querySelectorAll('.animate');
+
+      svgElements.forEach((el) => {
+        el.setAttributeNS(null, 'transform', transformValue);
+      });
+    };
+
     window.addEventListener('scroll', handleScroll);
 
     return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  }, [transform]);
 
   return (
+    // eslint-disable-next-line jsx-a11y/alt-text
     <object
       type="image/svg+xml"
       aria-hidden="true"

components/Firebase/app.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-import config from './config'
+import config from './config';
 
 // Singleton
 export default firebase.apps.length

components/Firebase/config.js
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: "AIzaSyBW2hLAzSgdv72lKicKcW_j1c86enCi8uU",
-  authDomain: "nj-career-network.firebaseapp.com",
-  databaseURL: "https://nj-career-network.firebaseio.com",
-  projectId: "nj-career-network",
-  storageBucket: "nj-career-network.appspot.com",
-  messagingSenderId: "114141088298",
-  appId: "1:114141088298:web:ce96ec93a41e3d35"
+  apiKey: 'AIzaSyBW2hLAzSgdv72lKicKcW_j1c86enCi8uU',
+  authDomain: 'nj-career-network.firebaseapp.com',
+  databaseURL: 'https://nj-career-network.firebaseio.com',
+  projectId: 'nj-career-network',
+  storageBucket: 'nj-career-network.appspot.com',
+  messagingSenderId: '114141088298',
+  appId: '1:114141088298:web:ce96ec93a41e3d35',
 };
 
 export default config;

components/Firebase/index.js
@@ -1,3 +1,7 @@
+// TODO: understand these rules and un-disable them:
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
+
 import FirebaseContext, { withFirebase } from './context';
 import Firebase from './firebase';
 

components/Firebase/uiConfig.js
@@ -3,16 +3,18 @@ import 'firebase/auth';
 
 const uiConfig = {
   callbacks: {
-    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+    signInSuccessWithAuthResult(authResult, redirectUrl) {
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
-      console.log('Signed in');
-      console.log('authResult:', authResult);
-      console.log('redirectUrl:', redirectUrl);
+
+      // TODO: remove
+      // eslint-disable-next-line no-console
+      console.log('Signed in', 'authResult:', authResult, 'redirectUrl:', redirectUrl);
+
       return true;
     },
-    uiShown: function () {
+    uiShown() {
       // The widget is rendered.
       // Hide the loader.
       // document.getElementById('loader').style.display = 'none';
@@ -33,8 +35,7 @@ const uiConfig = {
   // Terms of service url.
   tosUrl: '/terms-of-service',
   // Privacy policy url.
-  privacyPolicyUrl: '/privacy-policy'
-
+  privacyPolicyUrl: '/privacy-policy',
 };
 
 export default uiConfig;

components/Footer.js
@@ -7,7 +7,6 @@ import Typography from '@material-ui/core/Typography';
 import ScaffoldContainer from './ScaffoldContainer';
 
 const color = '#fff';
-const hoverColor = '';
 const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: '#333',
@@ -28,14 +27,14 @@ const useStyles = makeStyles(theme => ({
     },
   },
   text: {
-    color
+    color,
   },
   mobileCentered: {
     [theme.breakpoints.down('xs')]: {
       marginLeft: 'auto',
       marginRight: 'auto',
     },
-  }
+  },
 }));
 
 function Footer() {
@@ -62,7 +61,7 @@ function Footer() {
 
   const svg = pathData => (
     <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className={classes.social}>
-      <path d={pathData}></path>
+      <path d={pathData} />
     </svg>
   );
 
@@ -80,7 +79,10 @@ function Footer() {
 
   const copyright = (
     <Typography className={classes.text}>
-      Copyright &copy; Garden State Career Network, {year > 2019 && '2019–'}{year}
+      Copyright &copy; Garden State Career Network,
+      {' '}
+      {year > 2019 && '2019–'}
+      {year}
     </Typography>
   );
 
@@ -96,8 +98,8 @@ function Footer() {
     <footer className={classes.root}>
       <ScaffoldContainer>
         <Grid container spacing={spacing} alignItems="center" direction="row-reverse">
-          <Grid item xs={12} sm>{mobileCentered(social, "flex-end")}</Grid>
-          <Grid item xs={12} sm>{mobileCentered(copyright, "flex-start")}</Grid>
+          <Grid item xs={12} sm>{mobileCentered(social, 'flex-end')}</Grid>
+          <Grid item xs={12} sm>{mobileCentered(copyright, 'flex-start')}</Grid>
         </Grid>
       </ScaffoldContainer>
     </footer>

components/Header.js
@@ -3,18 +3,18 @@ import AppBar from '@material-ui/core/AppBar';
 import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 
-import Nav from './header/Nav'
+import Nav from './header/Nav';
 import User from './header/User';
 
 const useStyles = makeStyles(theme => ({
   root: {
   },
   navBar: {
-    backgroundColor: '#FFFFFF'
+    backgroundColor: '#FFFFFF',
   },
   userBar: {
-    backgroundColor: theme.palette.primary.main
-  }
+    backgroundColor: theme.palette.primary.main,
+  },
 }));
 
 function Header() {

commit 5ba2215768e051866810930572b41a34681a0720

Fix bug introduced in 29efcf994257208ae805b1050fe8262b5c3cd18b
The breaking change unbound the methods from .this -- the change was made to comport with linter rules, which have been updated to allow such syntax in 284dd60ced37e2b40a9327e4cc9526f0e88b3808

components/Firebase/firebase.js
@@ -12,11 +12,11 @@ class Firebase {
     this.uiConfig = uiConfig;
   }
 
-  signOut() {
+  signOut = () => {
     this.auth.signOut();
   }
 
-  onAuthStateChanged(callback) {
+  onAuthStateChanged = (callback) => {
     this.auth.onAuthStateChanged((authUser) => {
       // if (authUser) {
       //   this.user(authUser.uid)