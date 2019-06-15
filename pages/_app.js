567519e50e112075abd545883ccf93d5eda5fbdb
Ross Dakin <rossdakin@gmail.com>
2019-06-15 13:01:04 -0400

    Add page loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,24 @@ import { ThemeProvider } from '@material-ui/styles';
 import App, { Container } from 'next/app';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Head from 'next/head';
+import NProgress from 'nprogress';
 import React from 'react';
+import Router from 'next/router';
 
-import AuthProvider from '../components/Auth';
 import { SnackbarProvider } from '../components/Snackbar';
-import StateManager from '../components/StateManager';
+import AuthProvider from '../components/Auth';
 import Firebase, { FirebaseContext } from '../components/Firebase';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import StateManager from '../components/StateManager';
 import theme from '../src/theme';
+
+// eslint-disable-next-line no-unused-vars
+Router.events.on('routeChangeStart', (url) => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 class MyApp extends App {
   componentDidMount() {
