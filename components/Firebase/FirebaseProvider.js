16c3b41516d5be040d2f22f98d7723a017ef794a
Ross Dakin <rossdakin@gmail.com>
2019-06-21 03:30:24 -0400

    Don't actually need Rebase
    It looks like it only gives us some data binding magic (which we don't use in hooks anyway) and perhaps the "syncDoc" method (which doesn't seem all that reactive -- prefer to update a document in response to input, listen to changes to collections, update and components accordingly). Also trusting the Google SDK to be more performant, reliable, etc.

--- components/Firebase/FirebaseProvider.js
+++ components/Firebase/FirebaseProvider.js
@@ -3,7 +3,6 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Rebase from 're-base';
 
 import app from './app';
 import FirebaseContext from './FirebaseContext';
@@ -12,12 +11,10 @@ export default function FirebaseProvider(props) {
   const { children } = props;
   const { auth } = firebase;
   const db = firebase.firestore(app);
-  const base = Rebase.createClass(db);
 
   const value = {
     auth,
-    db, // should generally use 'base' instead, unless direct (lower-level) access needed
-    base,
+    db,
     userDocRef: db.collection('users').doc('fLveAGxSbvYej7ybx9g1qoOGfpo2'),
   };
 
