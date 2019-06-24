ca24608c5aa8b60df6e1d12c11408d9d0ec4e7ef
Ross Dakin <rossdakin@gmail.com>
2019-06-24 09:06:58 -0400

    Ensure negative checkbox values get recorded

--- components/assessment/Question/BinaryQuestion.js
+++ components/assessment/Question/BinaryQuestion.js
@@ -1,12 +1,19 @@
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import AirtablePropTypes from '../../Airtable/PropTypes';
 
 export default function BinaryQuestion(props) {
   const { onChange, question, value } = props;
+  const checkbox = useRef(null);
+
+  useEffect(() => {
+    // hack: persist check box values when they're first rendered,
+    // otherwise we don't get "false" recorded on checkboxes never touched.
+    onChange(checkbox.current.checked);
+  });
 
   return (
     <FormControlLabel
@@ -16,6 +23,7 @@ export default function BinaryQuestion(props) {
           color="primary"
           checked={value}
           value={question.id}
+          inputRef={checkbox}
           onChange={e => onChange(e.target.checked)}
         />
       )}
