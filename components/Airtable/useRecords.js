180fd980315f52a31eec1b2f8d82826276759bed
Ross Dakin <rossdakin@gmail.com>
2019-06-23 21:49:32 -0400

    Load all records from Airtable when request exceeds max page size (100 records)

--- components/Airtable/useRecords.js
+++ components/Airtable/useRecords.js
@@ -5,15 +5,37 @@ export default function useAirtable(apiPath) {
   const [records, setRecords] = useState([]);
   const cleanupRef = useRef();
 
+  const fetchJson = async (_apiPath, offset) => {
+    let path = _apiPath;
+
+    if (offset) {
+      const separator = path.includes('?') ? '&' : '?';
+      path += `${separator}offset=${offset}`;
+    }
+
+    cleanupRef.current = new Airtable().fetch(path);
+    const result = await cleanupRef.current;
+
+    cleanupRef.current = result.json();
+    const json = await cleanupRef.current;
+
+    return json;
+  };
+
   useEffect(() => {
     (async () => {
-      const result = await new Airtable().fetch(apiPath);
-      cleanupRef.current = result;
+      let offset = null;
+      let recordsAccum = [];
 
-      const json = await result.json();
-      cleanupRef.current = json;
+      do {
+        // eslint-disable-next-line no-await-in-loop
+        const result = await fetchJson(apiPath, offset);
 
-      setRecords(json.records);
+        ({ offset } = result);
+        recordsAccum = recordsAccum.concat(result.records);
+      } while (offset);
+
+      setRecords(recordsAccum);
     })();
 
     return () => {
