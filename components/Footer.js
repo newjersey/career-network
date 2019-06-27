c70eedfddd1fa7cbd83e1dabe82a0d79c5f46666
Ross Dakin <rossdakin@gmail.com>
2019-06-27 03:19:08 -0400

    Rebrand footer for pilot
    Fixes #160

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
+import NextLink from 'next/link';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
@@ -10,14 +11,8 @@ const color = '#fff';
 const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: '#333',
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-  },
-  logo: {
-    width: '144px',
-    [theme.breakpoints.up('sm')]: {
-      width: '160px',
-    },
+    paddingTop: theme.spacing(1.5),
+    paddingBottom: theme.spacing(1.5),
   },
   social: {
     width: '24px',
@@ -25,36 +20,34 @@ const useStyles = makeStyles(theme => ({
     '&:hover': {
       fill: '#CCC',
     },
+    position: 'relative',
+    top: 2,
   },
-  text: {
+  link: {
     color,
-  },
-  mobileCentered: {
-    [theme.breakpoints.down('xs')]: {
-      marginLeft: 'auto',
-      marginRight: 'auto',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
     },
   },
 }));
 
 function Footer() {
   const classes = useStyles();
-  const year = new Date().getFullYear();
-  const spacing = 1;
+  const links = [
+    {
+      name: 'Terms',
+      href: '/terms-of-service',
+    },
+    {
+      name: 'Privacy',
+      href: '/privacy-policy',
+    },
+  ];
   const socialIcons = [
     {
-      name: 'Twitter',
-      href: 'https://twitter.com/NJGov',
-      data: 'M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-48.9 158.8c.2 2.8.2 5.7.2 8.5 0 86.7-66 186.6-186.6 186.6-37.2 0-71.7-10.8-100.7-29.4 5.3.6 10.4.8 15.8.8 30.7 0 58.9-10.4 81.4-28-28.8-.6-53-19.5-61.3-45.5 10.1 1.5 19.2 1.5 29.6-1.2-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3a65.447 65.447 0 0 1-29.2-54.6c0-12.2 3.2-23.4 8.9-33.1 32.3 39.8 80.8 65.8 135.2 68.6-9.3-44.5 24-80.6 64-80.6 18.9 0 35.9 7.9 47.9 20.7 14.8-2.8 29-8.3 41.6-15.8-4.9 15.2-15.2 28-28.8 36.1 13.2-1.4 26-5.1 37.8-10.2-8.9 13.1-20.1 24.7-32.9 34z',
-    },
-    {
-      name: 'Facebook',
-      href: 'https://www.facebook.com/NJGov',
-      data: 'M448 80v352c0 26.5-21.5 48-48 48h-85.3V302.8h60.6l8.7-67.6h-69.3V192c0-19.6 5.4-32.9 33.5-32.9H384V98.7c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9H184v67.6h60.9V480H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48z',
-    },
-    {
       name: 'GitHub',
-      href: 'https://github.com/newjersey/',
+      href: 'https://github.com/newjersey/career-network/',
       data: 'M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM277.3 415.7c-8.4 1.5-11.5-3.7-11.5-8 0-5.4.2-33 .2-55.3 0-15.6-5.2-25.5-11.3-30.7 37-4.1 76-9.2 76-73.1 0-18.2-6.5-27.3-17.1-39 1.7-4.3 7.4-22-1.7-45-13.9-4.3-45.7 17.9-45.7 17.9-13.2-3.7-27.5-5.6-41.6-5.6-14.1 0-28.4 1.9-41.6 5.6 0 0-31.8-22.2-45.7-17.9-9.1 22.9-3.5 40.6-1.7 45-10.6 11.7-15.6 20.8-15.6 39 0 63.6 37.3 69 74.3 73.1-4.8 4.3-9.1 11.7-10.6 22.3-9.5 4.3-33.8 11.7-48.3-13.9-9.1-15.8-25.5-17.1-25.5-17.1-16.2-.2-1.1 10.2-1.1 10.2 10.8 5 18.4 24.2 18.4 24.2 9.7 29.7 56.1 19.7 56.1 19.7 0 13.9.2 36.5.2 40.6 0 4.3-3 9.5-11.5 8-66-22.1-112.2-84.9-112.2-158.3 0-91.8 70.2-161.5 162-161.5S388 165.6 388 257.4c.1 73.4-44.7 136.3-110.7 158.3zm-98.1-61.1c-1.9.4-3.7-.4-3.9-1.7-.2-1.5 1.1-2.8 3-3.2 1.9-.2 3.7.6 3.9 1.9.3 1.3-1 2.6-3 3zm-9.5-.9c0 1.3-1.5 2.4-3.5 2.4-2.2.2-3.7-.9-3.7-2.4 0-1.3 1.5-2.4 3.5-2.4 1.9-.2 3.7.9 3.7 2.4zm-13.7-1.1c-.4 1.3-2.4 1.9-4.1 1.3-1.9-.4-3.2-1.9-2.8-3.2.4-1.3 2.4-1.9 4.1-1.5 2 .6 3.3 2.1 2.8 3.4zm-12.3-5.4c-.9 1.1-2.8.9-4.3-.6-1.5-1.3-1.9-3.2-.9-4.1.9-1.1 2.8-.9 4.3.6 1.3 1.3 1.8 3.3.9 4.1zm-9.1-9.1c-.9.6-2.6 0-3.7-1.5s-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3 1.1 1.5 1.1 3.3 0 4.1zm-6.5-9.7c-.9.9-2.4.4-3.5-.6-1.1-1.3-1.3-2.8-.4-3.5.9-.9 2.4-.4 3.5.6 1.1 1.3 1.3 2.8.4 3.5zm-6.7-7.4c-.4.9-1.7 1.1-2.8.4-1.3-.6-1.9-1.7-1.5-2.6.4-.6 1.5-.9 2.8-.4 1.3.7 1.9 1.8 1.5 2.6z',
     },
   ];
@@ -78,28 +71,33 @@ function Footer() {
   );
 
   const copyright = (
-    <Typography variant="body2" className={classes.text}>
-      Copyright &copy; Garden State Career Network,
-      {' '}
-      {year > 2019 && '2019–'}
-      {year}
-    </Typography>
-  );
-
-  const mobileCentered = (content, nonMobileAlign) => (
-    <Grid container direction="column" alignItems={nonMobileAlign}>
-      <Grid item className={classes.mobileCentered}>
-        <Grid container spacing={spacing}>{content}</Grid>
+    <React.Fragment>
+      <Grid container spacing={2}>
+        {links.map(link => (
+          <Grid item key={link.href}>
+            <Typography variant="body2">
+              <NextLink href={link.href}>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a className={classes.link}>{link.name}</a>
+              </NextLink>
+            </Typography>
+          </Grid>
+        ))}
       </Grid>
-    </Grid>
+    </React.Fragment>
   );
 
   return (
     <footer className={classes.root}>
       <ScaffoldContainer>
-        <Grid container spacing={spacing} alignItems="center" direction="row-reverse">
-          <Grid item xs={12} sm={4}>{mobileCentered(social, 'flex-end')}</Grid>
-          <Grid item xs={12} sm={8}>{mobileCentered(copyright, 'flex-start')}</Grid>
+        <Grid
+          container
+          alignItems="center"
+          direction="row"
+          justify="space-between"
+        >
+          <Grid item>{copyright}</Grid>
+          <Grid item>{social}</Grid>
         </Grid>
       </ScaffoldContainer>
     </footer>
