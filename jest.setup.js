3b0716c172429aa63bbcd2ddf35582828dca849a
Abraham Sánchez <abraham@twintags.com>
2019-07-17 19:11:18 +0200

    Set up firebase application specific for tests
    It requires the Cloud Firestore emulator to be running.

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,9 @@
 import 'jest-dom/extend-expect';
 import '@testing-library/react/cleanup-after-each';
+import { clearFirestoreData } from '@firebase/testing';
+
+afterEach(async () => {
+  await clearFirestoreData({
+    projectId: 'nj-carrer-network-test',
+  });
+});
