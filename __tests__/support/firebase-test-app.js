f20def3870ed2376b4e0aabad53928953e937f08
Abraham Sánchez <abraham@twintags.com>
2019-08-01 13:51:50 +0200

    Bump dependencies to their latest versions

--- __tests__/support/firebase-test-app.js
+++ __tests__/support/firebase-test-app.js
@@ -1,15 +1,22 @@
 import fs from 'fs';
 import path from 'path';
+import firebase from 'firebase/app';
 import { initializeTestApp, loadFirestoreRules } from '@firebase/testing';
+import { env } from '../../next.config';
 
 const firebaseTestApp = initializeTestApp({
-  projectId: 'nj-career-network-test',
-  auth: { uid: 'test-user', email: 'test-user@example.com' },
+  projectId: env.firebase.projectId,
+  auth: {
+    uid: 'test-user',
+    email: 'test-user@example.com',
+  },
 });
 
 loadFirestoreRules({
-  projectId: 'nj-career-network-test',
+  projectId: env.firebase.projectId,
   rules: fs.readFileSync(path.resolve(__dirname, './firestore.test.rules'), 'utf8'),
 });
+
+firebase.initializeApp(env.firebase);
 
 export default firebaseTestApp;
