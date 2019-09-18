529ea7dcdcf276c7c2104a2b4733bb6d5158acb0
Ross Dakin <rossdakin@gmail.com>
2019-09-18 18:16:25 -0400

    Show radio buttons vertically in assessment
    Too many options to show horizontally (previously were only two)

--- components/assessment/Question/OptionQuestion/RadiosQuestion.js
+++ components/assessment/Question/OptionQuestion/RadiosQuestion.js
@@ -39,7 +39,6 @@ export default function RadiosQuestion(props) {
         onChange={e => onChange(e.target.value)}
         name={question.id}
         value={value}
-        row
       >
         {responseOptions.map(option => (
           <FormControlLabel
