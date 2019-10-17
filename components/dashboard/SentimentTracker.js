9f27f2a366888403a74895890a56bd18daa972c9
Ross Dakin <rossdakin@gmail.com>
2019-10-17 18:54:50 -0400

    Log sentiment events in Intercom

--- components/dashboard/SentimentTracker.js
+++ components/dashboard/SentimentTracker.js
@@ -65,6 +65,7 @@ const SentimentTracker = () => {
     };
 
     userDocRef.collection('sentimentEvents').add(data);
+    window.Intercom('trackEvent', 'logged-sentiment', sentiment);
     setHidden(true);
   };
 
