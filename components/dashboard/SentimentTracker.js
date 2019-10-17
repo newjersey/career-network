c7d4a6cc9d97005dd971eb1e86e3bc4a50a378b5
Ross Dakin <rossdakin@gmail.com>
2019-10-17 18:39:39 -0400

    Arrange imports

--- components/dashboard/SentimentTracker.js
+++ components/dashboard/SentimentTracker.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
+
 import { useAuth } from '../Auth';
 import useIsSentimentSubmittedToday from '../Firebase/useIsSentimentSubmittedToday';
 
