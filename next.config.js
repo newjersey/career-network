c8b8601cfc3d4541aedc18d678929ae5468fedf2
Ross Dakin <rossdakin@gmail.com>
2019-12-02 11:22:20 -0800

    Use @next/bundle-analyzer
    Mostly to analyze effect of polyfills (to be added), but also just useful to keep in mind.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: true });
 const appEnv = require('./src/AppEnv');
 
 let env;
@@ -190,7 +191,7 @@ try {
   throw new Error(`${err.message} – do you need to run 'npm run env:X'?`);
 }
 
-module.exports = {
+module.exports = withBundleAnalyzer({
   env,
   liveAirtableApiBase,
-};
+});
