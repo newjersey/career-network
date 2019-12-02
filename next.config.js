6bdb1e1caacd020bade998df7e4a31bc850fdfed
Ross Dakin <rossdakin@gmail.com>
2019-12-02 11:53:06 -0800

    Only analyze bundle size during full builds
    Avoids analyzing in local dev

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: true });
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 const appEnv = require('./src/AppEnv');
 
 let env;
