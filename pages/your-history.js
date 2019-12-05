0f56eff0ff6dbca458ac792d0c25670420229d4a
Hannah Smith <hannah@twobulls.com>
2019-12-05 14:38:33 -0500

    added call to userSubcollection

--- pages/your-history.js
+++ pages/your-history.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { fullyLoaded } from '../src/app-helper';
 import { useAuth, withAuthRequired } from '../components/Auth';
+import { useUserSubcollection } from '../components/Firebase';
 import YourHistory from '../components/your-history/YourHistory';
 import FullPageProgress from '../components/FullPageProgress';
 import withTitle from '../components/withTitle';
 
 function YourHistoryPage() {
   const { user } = useAuth();
-  const props = {
-    activities: [],
-  };
+  const allUserActivities = useUserSubcollection('activityLogEntries');
 
-  return fullyLoaded(user) ? <YourHistory {...props} /> : <FullPageProgress />;
+  return fullyLoaded(user, allUserActivities) ? (
+    <YourHistory activities={allUserActivities} />
+  ) : (
+    <FullPageProgress />
+  );
 }
 
 export default withAuthRequired(withTitle(YourHistoryPage, 'Your History'));
