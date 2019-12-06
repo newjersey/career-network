ee6444fba3b4cb395a0ac3e3c1aefdb1524c6491
Ross Dakin <rossdakin@gmail.com>
2019-12-05 23:59:23 -0700

    Sort and limit dashboard activity log at the query level

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,12 +8,18 @@ import Dashboard from '../components/dashboard/Dashboard';
 import FullPageProgress from '../components/FullPageProgress';
 import withTitle from '../components/withTitle';
 
+// Do not define in the render function, or a render loop will occur:
+const ACTIVITY_LOG_ENTRIES_QUERY_METHODS = [{ orderBy: ['timestamp', 'desc'] }, { limit: 3 }];
+
 function DashboardPage() {
   const { user } = useAuth();
   const allQuestionResponses = useUserSubcollection('questionResponses');
   const allActionDispositionEvents = useUserSubcollection('actionDispositionEvents');
   const allTaskDispositionEvents = useUserSubcollection('taskDispositionEvents');
-  const allActivityLogEntries = useUserSubcollection('activityLogEntries');
+  const recentActivityLogEntries = useUserSubcollection(
+    'activityLogEntries',
+    ACTIVITY_LOG_ENTRIES_QUERY_METHODS
+  );
   const recordProps = {
     allPredicates: useRecords('Predicates'),
     allConditions: useRecords('Conditions'),
@@ -32,7 +38,7 @@ function DashboardPage() {
       allQuestionResponses={allQuestionResponses}
       allActionDispositionEvents={allActionDispositionEvents}
       allTaskDispositionEvents={allTaskDispositionEvents}
-      allActivityLogEntries={allActivityLogEntries}
+      recentActivityLogEntries={recentActivityLogEntries}
       {...recordProps}
     />
   ) : (
