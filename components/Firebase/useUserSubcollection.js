b3fbf9dd3fa50e4b129d92131dd555f221618d57
Ross Dakin <rossdakin@gmail.com>
2019-12-05 23:58:03 -0700

    Allow querying/sorting/filtering/limiting of user subcollections

--- components/Firebase/useUserSubcollection.js
+++ components/Firebase/useUserSubcollection.js
@@ -2,7 +2,30 @@ import { useEffect, useRef, useState } from 'react';
 
 import useAuth from '../Auth/useAuth';
 
-export default function useUserSubcollection(subcollectionName) {
+// see: https://firebase.google.com/docs/reference/js/firebase.firestore.Query.html
+const SUPPORTED_QUERY_METHODS = [
+  'endAt',
+  'endBefore',
+  'limit',
+  'limitToLast',
+  'orderBy',
+  'startAfter',
+  'startAt',
+  'where',
+];
+
+/*
+ * Example usage:
+ *
+ * // Do not define in the render function, or a render loop will occur:
+ * const FRIENDS_QUERY_METHODS = [{ where: ['state', '==', 'CA'] }, { limit: 10 }];
+ *
+ * function MyComponent() {
+ *   const friends = useUserSubcollection('friends', FRIENDS_QUERY_METHODS);
+ *   ...
+ * }
+ */
+export default function useUserSubcollection(subcollectionName, queryMethods) {
   const cleanupRef = useRef();
   const { userDocRef } = useAuth();
   const [subcollection, setSubcollection] = useState(undefined);
@@ -10,7 +33,26 @@ export default function useUserSubcollection(subcollectionName) {
   useEffect(() => {
     (async () => {
       const collectionRef = userDocRef.collection(subcollectionName);
-      const unsubscribe = collectionRef.onSnapshot(querySnapshot => {
+      let query = collectionRef;
+
+      if (queryMethods) {
+        queryMethods.forEach(queryMethod => {
+          const methodName = Object.keys(queryMethod)[0];
+          let methodArgs = queryMethod[methodName];
+
+          if (!SUPPORTED_QUERY_METHODS.includes(methodName)) {
+            throw new Error(`Unsupported query method: ${methodName}`);
+          }
+
+          if (!Array.isArray(methodArgs)) {
+            methodArgs = [methodArgs];
+          }
+
+          query = query[methodName](...methodArgs);
+        });
+      }
+
+      const unsubscribe = query.onSnapshot(querySnapshot => {
         setSubcollection(querySnapshot.docs);
       });
 
@@ -22,7 +64,7 @@ export default function useUserSubcollection(subcollectionName) {
         cleanupRef.current();
       }
     };
-  }, [subcollectionName, userDocRef]);
+  }, [queryMethods, subcollectionName, userDocRef]);
 
   return subcollection;
 }
