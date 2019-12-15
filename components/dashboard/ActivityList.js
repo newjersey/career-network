20ee4d4b6c208df33848ec501f4334373ff139a5
Ross Dakin <rossdakin@gmail.com>
2019-12-14 23:30:50 -0500

    Simplify

--- components/dashboard/ActivityList.js
+++ components/dashboard/ActivityList.js
@@ -11,41 +11,35 @@ import FirebasePropTypes from '../Firebase/PropTypes';
 export default function ActivityList(props) {
   const { activities, completedTasks, limit } = props;
   const sorted = [
-    ...activities.map(a => ({
-      ...a.data(),
-      cardType: 'ACTIVITY',
+    ...activities.map(item => ({
+      timestamp: item.data().timestamp,
+      feedItemProps: {
+        cardType: 'ACTIVITY',
+        title: item.data().briefDescription,
+        subheader: item.data().activityTypeLabel,
+        date: item.data().dateCompleted,
+        timeSpentInMinutes: item.data().timeSpentInMinutes,
+        key: item.data().timestamp,
+      },
     })),
-    ...completedTasks.map(t => ({
-      ...t.data(),
-      cardType: 'TASK',
+    ...completedTasks.map(item => ({
+      timestamp: item.data().timestamp,
+      feedItemProps: {
+        cardType: 'TASK',
+        title: item.data().task.fields.Title,
+        subheader: item.data().task.fields.Category,
+        date: item.data().timestamp,
+        key: item.data().taskId,
+      },
     })),
   ].sort((a, b) => compareDesc(a.timestamp.toDate(), b.timestamp.toDate()));
 
   return (
     <div>
       {!sorted.length && <Typography color="textSecondary">None</Typography>}
-      {sorted
-        .slice(0, limit)
-        .map(item =>
-          item.cardType === 'ACTIVITY' ? (
-            <FeedCard
-              cardType={item.cardType}
-              title={item.briefDescription}
-              subheader={item.activityTypeLabel}
-              date={item.dateCompleted}
-              timeSpentInMinutes={item.timeSpentInMinutes}
-              key={item.timestamp}
-            />
-          ) : (
-            <FeedCard
-              cardType={item.cardType}
-              title={item.task.fields.Title}
-              subheader={item.task.fields.Category}
-              date={item.timestamp}
-              key={item.taskId}
-            />
-          )
-        )}
+      {sorted.slice(0, limit).map(item => (
+        <FeedCard {...item.feedItemProps} />
+      ))}
       {!!sorted.length && (
         <NextLink href="/progress">
           <Button color="primary" variant="contained" fullWidth>
