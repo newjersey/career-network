ea174627d3e2f8e9756a522accaa71a5a92016e8
Hannah Smith <hannah@twobulls.com>
2019-12-16 16:36:17 -0500

    Style Success message in modal

--- components/dashboard/SubmitSuccess.js
+++ components/dashboard/SubmitSuccess.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
 const SubmitSuccess = ({ message, show }) =>
   show ? (
-    <Grid container direction="column" justify="center" alignItems="center">
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      py={3}
+      mb={2}
+    >
       <CheckCircleIcon style={{ fontSize: 100, color: 'green' }} />
       <Typography variant="h4">{message}</Typography>
-    </Grid>
+    </Box>
   ) : null;
 
 SubmitSuccess.propTypes = {
