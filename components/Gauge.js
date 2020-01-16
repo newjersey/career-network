bc6f72caea06a3d9328431c0561486b319bf4a3f
Rachel Chen <rachel@twobulls.com>
2020-01-16 10:33:34 -0500

    Show empty state for confidence meter when there's no activities

--- components/Gauge.js
+++ components/Gauge.js
@@ -4,18 +4,23 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 function Gauge(props) {
-  const { diameter, stroke, strokeWidth, percentage, label } = props;
+  const { diameter, stroke, strokeWidth, percentage, label, disabled } = props;
   const calculatedPercent = Math.trunc(percentage * 100);
   const coordinateForCircle = diameter / 2;
   const radius = (diameter - 2 * strokeWidth) / 2;
   const circumference = Math.PI * radius;
-  const semiCirclePercentage = (calculatedPercent / 100) * circumference;
+  const semiCirclePercentage = disabled ? circumference : (calculatedPercent / 100) * circumference;
+
   return (
     <Box display="flex" justifyContent="center" alignItems="flex-end" position="relative">
       <svg width={diameter} height={diameter / 2} style={{ transform: 'rotateY(180deg)' }}>
         <defs>
           <linearGradient id="percentGradient" x1="0" y1="0" x2="1" y2="1">
             <stop offset="0%" stopColor={stroke} />
+            <stop offset="100%" stopColor="#ffffff" />
+          </linearGradient>
+          <linearGradient id="greyGradient" x1="0" y1="0" x2="1" y2="1">
+            <stop offset="0%" stopColor="#979797" />
             <stop offset="100%" stopColor="#ffffff" />
           </linearGradient>
         </defs>
@@ -38,7 +43,7 @@ function Gauge(props) {
           cy={coordinateForCircle}
           r={radius}
           fill="none"
-          stroke="url(#percentGradient)"
+          stroke={disabled ? 'url(#greyGradient)' : 'url(#percentGradient)'}
           strokeWidth={strokeWidth}
           strokeDasharray={circumference}
           style={{
@@ -49,14 +54,16 @@ function Gauge(props) {
           strokeLinecap="round"
         />
       </svg>
-      <Box position="absolute" width={1}>
-        <Typography variant="h5" align="center" style={{ fontWeight: 'bold' }}>
-          {calculatedPercent}%
-        </Typography>
-        <Typography variant="body2" align="center" color="textSecondary">
-          {label}
-        </Typography>
-      </Box>
+      {!disabled && (
+        <Box position="absolute" width={1}>
+          <Typography variant="h5" align="center" style={{ fontWeight: 'bold' }}>
+            {calculatedPercent}%
+          </Typography>
+          <Typography variant="body2" align="center" color="textSecondary">
+            {label}
+          </Typography>
+        </Box>
+      )}
     </Box>
   );
 }
@@ -67,12 +74,14 @@ Gauge.propTypes = {
   diameter: PropTypes.number,
   percentage: PropTypes.number.isRequired,
   label: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
 };
 
 Gauge.defaultProps = {
   stroke: '#1881c5',
   strokeWidth: 10,
   diameter: 218,
+  disabled: false,
 };
 
 export default Gauge;
