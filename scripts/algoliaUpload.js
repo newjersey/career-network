3c4ead30cca12c6e284525630f7e2cf1b1889440
Ross Dakin <rossdakin@gmail.com>
2020-02-06 08:59:30 -0500

    Revert "Update algoliaUpload script to create index for searching distinct occupation"
    This reverts commit 56e135f0af186969b3116e163ca2dcc90fd47613.

--- scripts/algoliaUpload.js
+++ scripts/algoliaUpload.js
@@ -11,61 +11,44 @@ const CSV_PATH = 'data/Employment Prospects data from NJ.csv';
 const API_KEY = process.env.ALGOLIA_ADMIN_API_KEY;
 const APP_ID = '3XON39SKZ0';
 const INDEX_NAME = 'prod_EMPLOYMENT_PROSPECTS';
-const DISTINCT_OCCUPATION_INDEX_NAME = 'DISTINCT_OCCUPATION';
 const UPLOAD_BATCH_COUNT = 1000;
 
 const client = algoliasearch(APP_ID, API_KEY);
-const indexMain = client.initIndex(INDEX_NAME);
-const indexDistinct = client.initIndex(DISTINCT_OCCUPATION_INDEX_NAME);
+const index = client.initIndex(INDEX_NAME);
 
-const addObjects = async (objects, index) => {
+const addObjects = async objects => {
   const { objectIDs, taskID } = await index.addObjects(objects);
   console.log(`Added ${objectIDs.length} objects (taskID: ${taskID})`);
   return index.waitTask(taskID);
 };
 
-const clearIndex = async index => {
+const clearIndex = async () => {
   // clear old objects
   const { updatedAt, taskID } = await index.clearIndex();
   console.log(`Clearing index at ${updatedAt} (taskID: ${taskID})`);
   return index.waitTask(taskID);
 };
 
-const setSettings = async index => {
+const setSettings = async () => {
   // set settings
-  if (index.indexName === DISTINCT_OCCUPATION_INDEX_NAME) {
-    const { updatedAt, taskID } = await index.setSettings({
-      searchableAttributes: ['Occupation'],
-      attributeForDistinct: 'Occupation',
-      distinct: 1,
-      ignorePlurals: true,
-      queryLanguages: ['en'],
-      indexLanguages: ['en'],
-    });
-    console.log(`Setting indexDistinct settings at ${updatedAt} (taskID: ${taskID})`);
-    return index.waitTask(taskID);
-  }
-  if (index.indexName === INDEX_NAME) {
-    const { updatedAt, taskID } = await index.setSettings({
-      searchableAttributes: ['Occupation'],
-      attributesForFaceting: [
-        'county',
-        'GrowthRates',
-        'Total Openings',
-        'Education',
-        'Work Exp_',
-        'meanann',
-        'Descriptor',
-        'DescriptorRating',
-      ],
-      ignorePlurals: true,
-      queryLanguages: ['en'],
-      indexLanguages: ['en'],
-    });
-    console.log(`Setting indexMain settings at ${updatedAt} (taskID: ${taskID})`);
-    return index.waitTask(taskID);
-  }
-  return Promise.resolve();
+  const { updatedAt, taskID } = await index.setSettings({
+    searchableAttributes: ['Occupation'],
+    attributesForFaceting: [
+      'county',
+      'GrowthRates',
+      'Total Openings',
+      'Education',
+      'Work Exp_',
+      'meanann',
+      'Descriptor',
+      'DescriptorRating',
+    ],
+    ignorePlurals: true,
+    queryLanguages: ['en'],
+    indexLanguages: ['en'],
+  });
+  console.log(`Setting index settings at ${updatedAt} (taskID: ${taskID})`);
+  return index.waitTask(taskID);
 };
 
 class Uploader extends Writable {
@@ -81,8 +64,7 @@ class Uploader extends Writable {
   }
 
   flushBuffer() {
-    this.tasks.push(addObjects(this.buffer, indexMain));
-    this.tasks.push(addObjects(this.buffer, indexDistinct));
+    this.tasks.push(addObjects(this.buffer));
     this.objCount += this.buffer.length;
     this.buffer = [];
   }
@@ -208,10 +190,8 @@ const transformer = transform((record, callback) => {
 console.log(`Updating Algolia app ${APP_ID}, index ${INDEX_NAME}...`);
 
 (async () => {
-  await clearIndex(indexMain);
-  await clearIndex(indexDistinct);
-  await setSettings(indexMain);
-  await setSettings(indexDistinct);
+  await clearIndex();
+  await setSettings();
 
   readStream
     .pipe(parser)
