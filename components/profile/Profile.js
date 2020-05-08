86a54666091eb3c7b0f344fb1d3cfae2982bb2ed
Rachel Chen <rachel@twobulls.com>
2020-05-08 14:04:34 -0400

    Add ProfileItemCard and UserProfileCard component
    Pull user info from assessment responses
    
    Update text style
    
    Update profile card to match the card in dashboard

--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -1,27 +1,131 @@
 import { makeStyles } from '@material-ui/styles';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
+import { useAuth } from '../Auth';
+import FirebasePropTypes from '../Firebase/PropTypes';
+import ProfileItemCard from './ProfileItemCard';
+import UserProfileCard from './UserProfileCard';
 import ScaffoldContainer from '../ScaffoldContainer';
 
-const useStyles = makeStyles(() => ({
+const ROW_GAP = 2;
+const COL_GAP = 2;
+
+const useStyles = makeStyles(theme => ({
   root: {
     position: 'relative',
   },
+  subtitle: {
+    display: 'inline-block',
+  },
+  container: {
+    marginTop: theme.spacing(-5),
+  },
+  grid: {
+    display: 'grid',
+    marginTop: theme.spacing(4),
+    gridGap: theme.spacing(ROW_GAP, COL_GAP),
+    gridTemplateAreas: `
+      'H'
+      'C'
+      'L'
+      'R'
+    `,
+
+    [theme.breakpoints.up('md')]: {
+      gridTemplateColumns: '1fr 2fr 1fr',
+      gridTemplateAreas: `
+        '. H .'
+        'L C R'
+      `,
+    },
+
+    /* All Following CSS exists to support IE 11. Keep in sync with the grid CSS above.
+     * Helpful tool: https://autoprefixer.github.io/
+     */
+    '-ms-grid-rows': `
+      auto
+      ${theme.spacing(ROW_GAP)}px
+      auto
+      ${theme.spacing(ROW_GAP)}px
+      auto
+      ${theme.spacing(ROW_GAP)}px
+      auto`,
+  },
+  gridH: { '-ms-grid-row': 1, '-ms-grid-column': 1, 'grid-area': 'H' },
+  gridC: { '-ms-grid-row': 3, '-ms-grid-column': 1, 'grid-area': 'C' },
+  gridL: { '-ms-grid-row': 5, '-ms-grid-column': 1, 'grid-area': 'L' },
+  gridR: { '-ms-grid-row': 7, '-ms-grid-column': 1, 'grid-area': 'R' },
+  [theme.breakpoints.up('md')]: {
+    grid: {
+      '-ms-grid-columns': `1fr ${theme.spacing(COL_GAP)}px 2fr ${theme.spacing(COL_GAP)}px 1fr`,
+      '-ms-grid-rows': `auto ${theme.spacing(ROW_GAP)}px auto`,
+    },
+    gridH: { '-ms-grid-row': 1, '-ms-grid-column': 3 },
+    gridC: { '-ms-grid-row': 3, '-ms-grid-column': 3 },
+    gridL: { '-ms-grid-row': 3, '-ms-grid-column': 1 },
+    gridR: { '-ms-grid-row': 3, '-ms-grid-column': 5 },
+  },
 }));
 
-function Profile() {
+const PROFILE_ITEMS = [
+  {
+    title: 'My Goal',
+    description: 'Edit Your Profile to add your Goal',
+  },
+  {
+    title: 'Education History',
+    description: 'Edit Your Profile to add Education History',
+  },
+  {
+    title: 'Employment Experience',
+    description: 'Edit Your Profile to add Employment Experience',
+  },
+];
+
+const PHONE_QUESTION_ID = 'recDkvWeqvMxahFT2';
+
+function Profile({ allQuestionResponses }) {
   const classes = useStyles();
+  const { user } = useAuth();
+  const questionResponse = allQuestionResponses.find(doc => doc.id === PHONE_QUESTION_ID);
+  const phoneNumber = questionResponse && questionResponse.data().value;
 
   return (
     <div className={classes.root}>
       <ScaffoldContainer>
-        <Typography component="h1" variant="h2" gutterBottom>
-          Profile
-        </Typography>
+        <Box className={classes.grid} mb={10}>
+          <Box className={classes.gridC}>
+            {PROFILE_ITEMS.map(item => (
+              <Box mb={3}>
+                <ProfileItemCard title={item.title} description={item.description} />
+              </Box>
+            ))}
+          </Box>
+          <Box className={classes.gridL} position="relative">
+            <UserProfileCard user={user} phoneNumber={phoneNumber} />
+          </Box>
+          <Box className={classes.gridR}>
+            <Card variant="outlined">
+              <CardContent>
+                <Typography variant="h6" gutterBottom>
+                  Support Services
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  NJ residents may be eligible for a variety of support services. Select the Support
+                  Services you are interested in getting more information about.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        </Box>
       </ScaffoldContainer>
     </div>
   );
 }
+Profile.propTypes = { allQuestionResponses: FirebasePropTypes.querySnapshot.isRequired };
 
 export default Profile;
--- pages/profile.js
+++ pages/profile.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import { fullyLoaded } from '../src/app-helper';
 import { useAuth, withAuthRequired } from '../components/Auth';
+import { useUserSubcollection } from '../components/Firebase';
 import Profile from '../components/profile/Profile';
 import FullPageProgress from '../components/FullPageProgress';
 import withTitle from '../components/withTitle';
 
 function ProfilePage() {
   const { user } = useAuth();
+  const allQuestionResponses = useUserSubcollection('questionResponses');
 
-  return fullyLoaded(user) ? <Profile /> : <FullPageProgress />;
+  return fullyLoaded(user, allQuestionResponses) ? (
+    <Profile allQuestionResponses={allQuestionResponses} />
+  ) : (
+    <FullPageProgress />
+  );
 }
 
 export default withAuthRequired(withTitle(ProfilePage, 'Profile'));
