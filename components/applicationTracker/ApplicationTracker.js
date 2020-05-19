b869e6ee5e1e9e06ccab07a30755ab7ac98b566a
Rachel Chen <rachel@twobulls.com>
2020-05-19 13:21:42 -0400

    Add initial application storage

--- components/applicationTracker/ApplicationTracker.js
+++ components/applicationTracker/ApplicationTracker.js
@@ -2,6 +2,9 @@ import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import firebase from 'firebase/app';
+
+import { useAuth } from '../Auth';
 import ScaffoldContainer from '../ScaffoldContainer';
 import BackgroundHeader from '../BackgroundHeader';
 
@@ -21,15 +24,81 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const APPLICATION_STATUS_TYPES = [
+  {
+    value: 'submitted',
+    label: 'Application Submitted',
+  },
+  {
+    value: 'phone-screen',
+    label: 'Phone Screen',
+  },
+  {
+    value: 'interview',
+    label: 'Interviews',
+  },
+  {
+    value: 'offer-received',
+    label: 'Offer Received',
+  },
+  {
+    value: 'in-negation',
+    label: 'In Negation',
+  },
+  {
+    value: 'offer-accepted',
+    label: 'Offer Accepted',
+  },
+  {
+    value: 'offer-rejected',
+    label: 'Offer Rejected',
+  },
+];
+
+export async function logApplication(userDocRef, applicationDetails) {
+  const data = {
+    config: {
+      applicationStatusTypes: APPLICATION_STATUS_TYPES,
+    },
+    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    ...applicationDetails,
+  };
+
+  return userDocRef.collection('applicationLogEntries').add(data);
+}
+
 export default function ApplicationTracker() {
   const classes = useStyles();
+  const { userDocRef } = useAuth();
+
+  const handleSave = async () => {
+    const entry = {
+      id: 1,
+      notes: 'i liked the recruiter, jody',
+      status: 'application_submitted',
+    };
+    const data = {
+      jobTitle: 'software engineer',
+      company: 'two bulls',
+      dateApplied: new Date(), // should be user input
+      entries: [entry],
+      currentEntryId: 1,
+    };
+
+    try {
+      await logApplication(userDocRef, data);
+      console.log('success');
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <div className={classes.root}>
       <BackgroundHeader className={classes.backgroundHeader}>
         <ScaffoldContainer className={classes.header}>
           <Typography variant="h5">Application Tracker</Typography>
-          <Button className={classes.button} variant="contained" size="large">
+          <Button className={classes.button} onClick={handleSave} variant="contained" size="large">
             + Add Application
           </Button>
         </ScaffoldContainer>
