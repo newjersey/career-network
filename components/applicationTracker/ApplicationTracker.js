be58755be64a8261eba3ce886a2a49cf8ae5e795
Rachel Chen <rachel@twobulls.com>
2020-05-26 14:56:51 -0400

    Cleanup

--- components/applicationTracker/ApplicationTracker.js
+++ components/applicationTracker/ApplicationTracker.js
@@ -152,7 +152,7 @@ export default function ApplicationTracker({ allApplicationLogEntries }) {
       <ScaffoldContainer>
         <Paper className={classes.tableCard}>
           <Typography variant="h6" gutterBottom color="textPrimary">
-            Active applications ({activeApplicationCount})
+            Active Applications ({activeApplicationCount})
           </Typography>
           <ApplicationTable
             applications={activeApplications}
