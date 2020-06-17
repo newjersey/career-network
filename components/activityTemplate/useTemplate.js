cf1d14515a69383d37e18a1661509f2c3b02e376
Hannah Smith <hannah@twobulls.com>
2020-06-17 11:18:00 -0400

    Add service to dynamically retrieve template

---
+++ components/activityTemplate/useTemplate.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function useTemplate(templateId) {
+  const [template, setTemplate] = useState({});
+  const cleanupRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const result = await import(`../../activity-templates/${templateId}.json`);
+
+      if (!cleanupRef.current) {
+        setTemplate(result.default);
+      }
+    })();
+
+    return () => {
+      cleanupRef.current = true;
+    };
+  }, [templateId]);
+
+  return template;
+}
