b8dc4d9567cbaf8f3774458327b0a75f3eed3ab8
Hannah Smith <hannah@twobulls.com>
2020-06-22 08:06:04 -0400

    Add tips for success list block with formatting

--- components/activityTemplate/components/ListBlock.js
+++ components/activityTemplate/components/ListBlock.js
@@ -1,32 +1,43 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: theme.spacing(16),
+    paddingBottom: theme.spacing(16),
+    backgroundColor: props => props.backgroundColor || theme.palette.background.paper,
+  },
+}));
+
 const getItemKey = index => `item-${index}`;
 
-const ListBlock = ({ heading, subheading, items }) => (
-  <div>
-    {heading && (
-      <Typography gutterBottom display="block" variant="body1" style={{ fontWeight: 700 }}>
-        {heading}
-      </Typography>
-    )}
-    {subheading && (
-      <Typography paragraph display="block" variant="body2">
-        {subheading}
-      </Typography>
-    )}
-    <ul>
-      {items.map((item, index) => (
-        <Typography component="li" variant="body2" key={getItemKey(index)} gutterBottom>
-          <span dangerouslySetInnerHTML={{ __html: item }} />
+function ListBlock({ heading, subheading, items }) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      {heading && (
+        <Typography gutterBottom display="block" variant="body1" style={{ fontWeight: 700 }}>
+          {heading}
         </Typography>
-      ))}
-    </ul>
-  </div>
-);
-
+      )}
+      {subheading && (
+        <Typography paragraph display="block" variant="body2">
+          {subheading}
+        </Typography>
+      )}
+      <ul>
+        {items.map((item, index) => (
+          <Typography component="li" variant="body2" key={getItemKey(index)} gutterBottom>
+            <span dangerouslySetInnerHTML={{ __html: item }} />
+          </Typography>
+        ))}
+      </ul>
+    </div>
+  );
+}
 ListBlock.propTypes = {
   heading: PropTypes.string,
   subheading: PropTypes.string,
