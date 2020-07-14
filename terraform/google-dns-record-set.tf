976ac2de8f931b997e370bfc8b5d2b7a967c70e8
Ross Dakin <rossdakin@gmail.com>
2020-07-14 17:26:11 -0400

    Add D4AD (training.njcareers.org) to terraform DNS

--- terraform/google-dns-record-set.tf
+++ terraform/google-dns-record-set.tf
@@ -97,6 +97,53 @@ resource "google_dns_record_set" "txt" {
   ]
 }
 
+resource "google_dns_record_set" "a_training" {
+  name         = "training.${google_dns_managed_zone.default.dns_name}"
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = [
+    "216.239.32.21",
+    "216.239.34.21",
+    "216.239.36.21",
+    "216.239.38.21",
+  ]
+}
+
+resource "google_dns_record_set" "aaaa_training" {
+  name         = "training.${google_dns_managed_zone.default.dns_name}"
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "AAAA"
+  ttl          = 300
+
+  rrdatas = [
+    "2001:4860:4802:32::15",
+    "2001:4860:4802:34::15",
+    "2001:4860:4802:36::15",
+    "2001:4860:4802:38::15",
+  ]
+}
+
+resource "google_dns_record_set" "cname_www_training" {
+  name         = "www.training.${google_dns_managed_zone.default.dns_name}"
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "CNAME"
+  ttl          = 3600
+  rrdatas      = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "txt__training" {
+  name         = "training.${google_dns_managed_zone.default.dns_name}"
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "TXT"
+  ttl          = 300
+
+  rrdatas = [
+    "\"google-site-verification=a_-VsRnOAduarq6lHShBVJjSAHTpocPnt0eJPEWsrW0\"",
+  ]
+}
+
 resource "google_dns_record_set" "txt__dmarc" {
   name         = "_dmarc.${google_dns_managed_zone.default.dns_name}"
   managed_zone = google_dns_managed_zone.default.name
