31c101a49fdbfbf8562d83ce7af4099c2ac7e073
Ross Dakin <rossdakin@gmail.com>
2021-05-13 13:26:50 -0700

    Update Terraform syntax and key

--- terraform/google.tf
+++ terraform/google.tf
@@ -1,6 +1,6 @@
 provider "google" {
   # https://console.cloud.google.com/iam-admin/serviceaccounts
-  credentials = "${file("credentials/nj-career-network-9b89423c38b1.json")}"
+  credentials = file("credentials/nj-career-network-7e8a2bf86959.json")
   project     = "nj-career-network"
   region      = "nam5"
 }
